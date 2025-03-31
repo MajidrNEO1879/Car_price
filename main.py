6009178f8f108a7f25115652a133a13dc9aed6ae
MajidrNEO1879 <majidrezaeiphy1879@gmail.com>
2025-03-31 18:16:17 +0100

    testing the simple api for a simple linear model

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 app = Flask(__name__)
 
 # Load the trained model
-model = joblib.load("Car_price.pkl")
+model = joblib.load("car_encoded_model.pkl")
 
 @app.route("/")
 def home():
@@ -13,13 +13,11 @@ def home():
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    try:
-        data = request.get_json()
-        features = np.array(data["features"]).reshape(1, -1)  # Ensure correct shape
-        prediction = model.predict(features)
-        return jsonify({"prediction": prediction.tolist()})
-    except Exception as e:
-        return jsonify({"error": str(e)})
+    data = request.json 
+    X_test = np.array(data["features"]).reshape(1, -1) 
+
+    predicted = model.predict(X_test)
+    return jsonify({"prediction": predicted.tolist()})
 
 if __name__ == "__main__":
-    app.run(debug=True) 
+    app.run(debug=False)
